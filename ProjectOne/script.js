3757a0c53fc6e81dd2e05266fa4cde7a5afa9579
shaberry <jaden.png02@gmail.com>
2024-04-21 20:06:55 -0700

    updated

--- ProjectOne/script.js
+++ ProjectOne/script.js
@@ -8,7 +8,17 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+// BUTTONS
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+// STATS
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
 // let inventory = ["stick","dagger","sword"];
